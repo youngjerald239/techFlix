cd5e6e9ab0a71e7a387a0160612cbef852b1cae3
youngjerald239 <youngjerald239@gmail.com>
2021-07-06 17:55:27 -0500

    created requests component

--- src/requests.js
+++ src/requests.js
@@ -1,1 +1,14 @@
 const API_KEY = "f69586fa23917428cf2cd2aa82cf0f13"
+
+const requests = {
+    fetchTrending: `/trending/all/wee?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests
