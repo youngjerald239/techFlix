393ee5c5475bb53ed3111ad14ccf05afaa8e83e8
youngjerald239 <youngjerald239@gmail.com>
2021-07-06 18:00:02 -0500

    set up instance of axios

--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@ const API_KEY = "f69586fa23917428cf2cd2aa82cf0f13"
 
 const requests = {
     fetchTrending: `/trending/all/wee?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
