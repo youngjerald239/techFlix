4f0c4aacaf3bbbd60a72e0706381366c1d01c898
youngjerald239 <youngjerald239@gmail.com>
2021-07-07 14:10:24 -0500

    added css to homepage

--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,7 @@
 const API_KEY = "f69586fa23917428cf2cd2aa82cf0f13"
 
 const requests = {
-    fetchTrending: `/trending/all/wee?api_key=${API_KEY}&language=en-US`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
