bf35412ee53e82928ef396b257c20916ebf30a67
youngjerald239 <youngjerald239@gmail.com>
2021-07-06 23:45:42 -0500

    added rows and row component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from "react"
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Time to work on the front end!</h1>
+      <Row title="TECHFLIX ORIGINALS"/>
+      <Row title="Trending Now"/>
+      <Row title="Top Rated"/>
+      <Row title="Action Movies"/>
+      <Row title="Comedy Movies"/>
+      <Row title="Romance Movies"/>
+      <Row title="Horror Movies"/>
     </div>
   );
 }
