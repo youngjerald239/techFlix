8c259c00cfb7f9a897e2c655b889320ff3e7b67a
youngjerald239 <youngjerald239@gmail.com>
2021-07-07 00:11:07 -0500

    added rows and map api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 import React from "react"
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>Time to work on the front end!</h1>
-      <Row title="TECHFLIX ORIGINALS"/>
-      <Row title="Trending Now"/>
-      <Row title="Top Rated"/>
-      <Row title="Action Movies"/>
-      <Row title="Comedy Movies"/>
-      <Row title="Romance Movies"/>
-      <Row title="Horror Movies"/>
+      <Row title="TECHFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from './axios'
 
-function Row({title}) {
+function Row({title, fetchUrl}) {
+
+    const [movies, setMovies] = useState([])
+
+    // A snippet of code which runs based on a specific condition/variable
+    useEffect(() => {
+        //if [], run once when the row loads, and dont run again
+        async function fetchData() {
+            const request = await axios.get(fetchUrl)
+            // console.log(request.data.results)
+            setMovies(request.data.results)
+            return request
+            // "https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213"
+        }
+        fetchData()
+    }, [fetchUrl])
+
+    console.log(movies)
+
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
-             {/* title */}
-
+             
+            <div className="row__posters">
+                {/* <several row__poster(s)/> */}
+                {movies.map()}
+            </div>
              {/* container -> posters */}
         </div>
     )
