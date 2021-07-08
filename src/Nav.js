9a3f0c3854ae699f4f596da5f110635261bdf61e
youngjerald239 <youngjerald239@gmail.com>
2021-07-08 15:03:47 -0500

    added useEffect and useState show function

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./Nav.css"
 
 function Nav() {
+    const [show, handleShow] = useState(false)
+    
+    useEffect(() => {
+        //event listener for nav scroll
+       window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+            //handleShow(true)
+        } else handleShow(false)
+       })
+       return () => {
+           window.removeEventListener("scroll")
+       }
+    }, [])
+
     return (
         <div className="nav">
             <img className="nav__logo" src="https://i.imgur.com/hpmrt4U.png?2" alt="Techflix Logo"/>
