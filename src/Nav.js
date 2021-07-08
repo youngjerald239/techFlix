f6607b39e6319bf1843504ee7acc4f0349059ed1
youngjerald239 <youngjerald239@gmail.com>
2021-07-08 14:40:42 -0500

    added new logo avatar

--- src/Nav.js
+++ src/Nav.js
@@ -5,6 +5,7 @@ function Nav() {
     return (
         <div className="nav">
             <img className="nav__logo" src="https://i.imgur.com/hpmrt4U.png" alt="Techflix Logo"/>
+            <img className="nav__avatar" src="https://i.imgur.com/Myw3Gmu.png?1" alt="Techflix Logo"/>
         </div>
     )
 }
