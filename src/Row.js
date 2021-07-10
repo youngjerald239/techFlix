122516f35dd0ba8774ffdfef9e8c5fdcd3a7dd58
youngjerald239 <youngjerald239@gmail.com>
2021-07-09 19:37:37 -0500

    created onClick event for movie trailers

--- src/Row.js
+++ src/Row.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import axios from './axios'
 import "./Row.css"
 import YouTube from "react-youtube"
+import movieTrailer from 'movie-trailer'
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
@@ -38,10 +39,10 @@ function Row({title, fetchUrl, isLargeRow}) {
         if (trailerUrl) {
             setTrailerUrl('')
         } else {
-            movieTailer(movie?.name || "")
+            movieTrailer(movie?.name || "")
             .then(url => {
                 const urlParams = new URLSearchParams (new URL(url).search)
-                setTrailerUrl(urlParams.get('v'))
+                setTrailerUrl(urlParams.get("v"))
             }).catch(error => console.log(error))
         }
     }
@@ -56,7 +57,7 @@ function Row({title, fetchUrl, isLargeRow}) {
                    <img key={movie.id} onClick= {() => handleClick(movie)} className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
                 ))}
             </div>
-            {trailerUrl} && <YouTube videoId={trailerUrl} opts={opts}/>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>
              
         </div>
     )
