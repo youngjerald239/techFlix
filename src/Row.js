0698a1a0e4d64239bd3893ffce4ab85f7a2bcad6
youngjerald239 <youngjerald239@gmail.com>
2021-07-09 19:17:50 -0500

    added video options and styled the trailer box width and height

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from './axios'
 import "./Row.css"
+import YouTube from "react-youtube"
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
@@ -16,10 +17,19 @@ function Row({title, fetchUrl, isLargeRow}) {
             // console.log(request.data.results)
             setMovies(request.data.results)
             return request
-            // "https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213"
+            
         }
         fetchData()
     }, [fetchUrl])
+
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            // https://
+            autoplay: 1,
+        }
+    }
 
     console.log(movies)
 
@@ -33,6 +43,7 @@ function Row({title, fetchUrl, isLargeRow}) {
                    <img key={movie.id} className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
                 ))}
             </div>
+            <YouTube videoId={trailerUrl} opts={opts}/>
              {/* container -> posters */}
         </div>
     )
