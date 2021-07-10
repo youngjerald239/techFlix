0596081666d0673361f0024f8ef011d0aa6fb487
youngjerald239 <youngjerald239@gmail.com>
2021-07-09 19:32:52 -0500

    created set trailer url params

--- src/Row.js
+++ src/Row.js
@@ -8,6 +8,7 @@ const base_url = "https://image.tmdb.org/t/p/original/"
 function Row({title, fetchUrl, isLargeRow}) {
 
     const [movies, setMovies] = useState([])
+    const [trailerUrl, setTrailerUrl] = useState("")
 
     // A snippet of code which runs based on a specific condition/variable
     useEffect(() => {
@@ -33,6 +34,18 @@ function Row({title, fetchUrl, isLargeRow}) {
 
     console.log(movies)
 
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('')
+        } else {
+            movieTailer(movie?.name || "")
+            .then(url => {
+                const urlParams = new URLSearchParams (new URL(url).search)
+                setTrailerUrl(urlParams.get('v'))
+            }).catch(error => console.log(error))
+        }
+    }
+
     return (
         <div className="row">
             <h2>{title}</h2>
@@ -40,11 +53,11 @@ function Row({title, fetchUrl, isLargeRow}) {
             <div className="row__posters">
                 {/* <several row__poster(s)/> */}
                 {movies.map(movie => (
-                   <img key={movie.id} className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+                   <img key={movie.id} onClick= {() => handleClick(movie)} className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
                 ))}
             </div>
-            <YouTube videoId={trailerUrl} opts={opts}/>
-             {/* container -> posters */}
+            {trailerUrl} && <YouTube videoId={trailerUrl} opts={opts}/>
+             
         </div>
     )
 }
